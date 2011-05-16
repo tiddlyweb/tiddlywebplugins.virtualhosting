b540298eb33e4f0f6911e52cac6d0acd157bef77
Chris Dent <chris.dent@gmail.com>
2011-05-16 13:05:14 +0100

    release 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ AUTHOR = 'Chris Dent'
 AUTHOR_EMAIL = 'cdent@peermore.com'
 NAME = 'tiddlywebplugins.virtualhosting'
 DESCRIPTION = 'Enable virtualhosts from the same tiddlyweb server.'
-VERSION = '0.1'
+VERSION = '0.2'
 
 
 import os
